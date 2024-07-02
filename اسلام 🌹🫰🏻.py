9fc7491e0dc37995d2d843364ad213d4abdc98a6
skejexu <jwbevjsbwbsbhwk@gmail.com>
2024-07-02 21:42:09 +0300

    Update اسلام 🌹🫰🏻.py

--- اسلام 🌹🫰🏻.py
+++ اسلام 🌹🫰🏻.py
@@ -1,12 +1,4 @@
-
-تم توقيف اداء اسلام الجوكر
-
-
-
 import requests,bs4,json,os,sys,random,datetime,time,re
-
-
-
 import urllib3,rich,base64
 from rich.table import Table as me
 from rich.console import Console as sol
