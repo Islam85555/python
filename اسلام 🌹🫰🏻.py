f55f3b719b68bdef8b5ba88eaa2af362a0a3a995
skejexu <jwbevjsbwbsbhwk@gmail.com>
2024-07-02 21:34:45 +0300

    Update اسلام 🌹🫰🏻.py

--- اسلام 🌹🫰🏻.py
+++ اسلام 🌹🫰🏻.py
@@ -1,4 +1,4 @@
-import requests,bs4,json,os,sys,random,datetime,time,re
+import requests,bs4,jsonاسلامعليكم,os,sys,random,datetime,time,re
 import urllib3,rich,base64
 from rich.table import Table as me
 from rich.console import Console as sol
